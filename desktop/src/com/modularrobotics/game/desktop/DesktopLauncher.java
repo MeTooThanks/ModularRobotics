0ace4d58980f70e12338c15ed117d210abd02851
MeTooThanks <h.ritter1992@gmail.com>
2017-03-21 15:07:14 +0100

    Initial

---
+++ desktop/src/com/modularrobotics/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.modularrobotics.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.modularrobotics.game.ModularRobotics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ModularRobotics(), config);
+	}
+}
