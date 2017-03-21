40379c61feeccba0d332df5130e05ede9624baa3
MeTooThanks <h.ritter1992@gmail.com>
2017-03-21 15:22:02 +0100

    Tidied up

--- core/AiHandlerInterface.java
+++
@@ -1,7 +0,0 @@
-package com.modularrobotics.game;
-
-import java.util.ArrayList;
-
-public interface AiHandlerInterface {
-	ArrayList<PathfindingNode> pollPath(PathfindingNode start, PathfindingNode target);
-}
--- core/Module.java
+++
@@ -1,5 +0,0 @@
-package com.modularrobotics.game;
-
-public class Module {
-
-}
