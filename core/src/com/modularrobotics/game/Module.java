b5a9933979e1265d37d29ee6ce3ff2070ddf0402
snelj <sneljoanne@gmail.com>
2017-03-21 17:07:51 +0100

    modules

--- core/src/com/modularrobotics/game/Module.java
+++ core/src/com/modularrobotics/game/Module.java
@@ -1,5 +1,29 @@
 package com.modularrobotics.game;
 
-public class Module {
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
 
+
+public class Module extends ModelInstance {
+	
+	   Vector3 center;
+	    BoundingBox bounds;
+	    Vector3 dimensions;
+	    float radius;
+
+	    public Module(Model model, float x, float y, float z) {
+	        super(model,x ,y, z);
+
+	        center = new Vector3();
+	        bounds = new BoundingBox();
+	        dimensions = new Vector3();
+	        calculateBoundingBox(bounds);
+
+	        bounds.getCenter(center);
+	        bounds.getDimensions(dimensions);
+
+	        radius = dimensions.len()/2f;
+	    }
 }
